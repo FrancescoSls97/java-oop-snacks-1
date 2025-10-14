3e25d2812cf438dfc4ae33c29af4ef83317eb600
Francesco Sales <frankds1997@gmail.com>
2025-10-14 11:24:17 +0200

    snack 1

--- src/org/lesson/java/studenti/Studente.java
+++ src/org/lesson/java/studenti/Studente.java
@@ -1,5 +1,21 @@
 package org.lesson.java.studenti;
-public class Studente {
 
-   
+public class Studente {
+    // attributi della classe
+    private String nome;
+    private String cognome;
+    private int età;
+
+    // costruttore
+    public Studente(String nome, String cognome, int età) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.età = età;
+    }
+
+    // metodi per ottenere una stringa con tutti i dati
+    public String datiStudente() {
+        return nome + " " + cognome + " " + età + " anni";
+    }
+
 }
